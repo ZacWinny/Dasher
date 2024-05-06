9355ff3cd30c44f7fca309fee7595265a52b029d
ZacWinny <43806529+ZacWinny@users.noreply.github.com>
2024-05-07 09:17:07 +1000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -1,22 +1,20 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy import Integer, Column, String
-from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy import Integer, Column, String, Date, Time, Float
+
 
 class User(db.Model, UserMixin):
     id = db.Column(Integer, primary_key=True)
     email = db.Column(String(120), unique=True, nullable=False)
-    password_hash = db.Column(String(128), nullable=False)
+    password = db.Column(String(128), nullable=False)
     name = db.Column(String(64), nullable=True)
-    type = db.Column(String(64), nullable=False)
+    user_type = db.Column(String(64), nullable=False)
 
-    def __init__(self, email, password, type):
+    def __init__(self, email, password, user_type):
         self.email = email
-        self.password_hash = generate_password_hash(password)
-        self.type = type
+        self.password = password
+        self.user_type = user_type
 
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 class Customer(User):
     __tablename__ = 'customers'  # Define separate table name
@@ -25,6 +23,7 @@ class Customer(User):
         super().__init__(email, password, type)
         self.name = name
 
+
 class Restaurant(User):
     __tablename__ = 'restaurants'  # Define separate table name
 
@@ -32,3 +31,37 @@ class Restaurant(User):
         super().__init__(email, password, type)
         self.name = name
 
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    order_num = db.Column(Integer, primary_key=True, autoincrement=True)
+    customer_num = db.Column(Integer, nullable=False)
+    restaurant_num = db.Column(Integer, nullable=False)
+    order_date = db.Column(Date, nullable=False)
+    delivery_date = db.Column(Date, nullable=False)
+    delivery_time = db.Column(Time, nullable=False)
+    total = db.Column(Float, nullable=False)
+    status = db.Column(String(50), nullable=False)
+
+
+class Feedback(db.Model):
+    __tablename__ = 'feedback'
+
+    feedback_num = db.Column(Integer, primary_key=True, autoincrement=True)
+    order_num = db.Column(Integer, nullable=False)
+    customer_name = db.Column(String(50), nullable=False)
+    restaurant_num = db.Column(Integer, nullable=False)
+    feedback_date = db.Column(Date, nullable=False)
+    feedback_text = db.Column(String(500), nullable=False)  #
+    rating = db.Column(Integer, nullable=False)
+
+
+class FoodItem(db.Model):
+    __tablename__ = 'food_items'
+
+    food_num = db.Column(Integer, primary_key=True, autoincrement=True)
+    restaurant_num = db.Column(Integer, nullable=False)
+    name = db.Column(String(50), nullable=False)
+    price = db.Column(Float, nullable=False)
+    category = db.Column(String(50), nullable=False)
